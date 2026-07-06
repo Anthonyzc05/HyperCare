50922c56e6e1d46266492339b3f5d2dbd7642936
Anthonyzc05 <anthonyzuca052006@gmail.com>
2026-07-05 23:55:17 -0500

    Arreglo de bug

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+require("./src/config/db");
+
+const usuariosRoutes = require("./src/routes/usuariosRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/usuarios", usuariosRoutes);
-app.use("/api/reportes", reportesRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API Portal HTA funcionando correctamente");
+  res.json({
+    mensaje: "Backend funcionando correctamente"
+  });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en puerto ${PORT}`);
